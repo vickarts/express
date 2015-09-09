b3824a86a43a61cbc1346ff6b50d75d33eb5273d
Chad Vick <vickartsdesign@gmail.com>
2015-09-09 17:02:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+    res.send('Hello Express');
+});
+
+app.get('/me', function(req,res) {
+    res.send('@vickartsdesign');
+});
+
+app.get('/who/:name?', function(req,res) {
+    var name = req.params.name;
+    res.send(name + ' was here');
+});
+
+app.get('/who/:name?/:title', function(req,res) {
+    var name = req.params.name;
+    var title = req.params.title;
+    res.send('<b>Name:</b> ' + name + '<br>' + '<b>Title:</b> ' + title);
+});
+
+app.get('*', function(req,res) {
+    res.send('Bad Route');
+});
+
+var server = app.listen(3000, function() {
+    console.log('listening on port 3000');
+});
